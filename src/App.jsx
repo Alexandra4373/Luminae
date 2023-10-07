29b7649bf85b1b381a4411d6591e2cf87693e909
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-07 20:37:52 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, About, Blog } from "./containers";
+// import { Home } from "./containers/Home";
+import { About, Blog } from "./containers";
 
 function App() {
   return (
     <>
       <React.Fragment>
         <Routes>
-          <Route path="" element={<Home />} />
+          {/* <Route path="" element={<Home />} /> */}
           <Route path="/about" element={<About />} />
           <Route path="/blog" element={<Blog />} />
         </Routes>
