fa30f5f06f2e2bf1803f0ae942654a75428ae685
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-09 12:40:16 +0000

    Create SubCategories.jsx

---
+++ src/components/SubCategories.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const SubCategories = () => {
+  return (
+    <div>
+      <div className="bg-white text-black">
+        <div className="">
+          <h5>SHOE & BAG</h5>
+          <p>Casual Shoes</p>
+          <p>Boots</p>
+          <p>Sandals</p>
+          <p>Slippers</p>
+        </div>
+
+        <div className="">
+          <h5>
+            LUXURY & <br />
+            DESIGNER
+          </h5>
+          <p>Towels</p>
+          <p>Bathroom Sales</p>
+          <p>Bath Mats</p>
+          <p>Shower Caps</p>
+        </div>
+
+        <div className="">
+          <h5>HOME TEXTILE</h5>
+          <p>Bedding</p>
+          <p>Pillows</p>
+          <p>Handkerchief Towels</p>
+          <p>Curtain</p>
+        </div>
+
+        <div className="">
+          <h5>COSMETICS</h5>
+          <p>Shampoo and Conditioner</p>
+          <p>Styling Products</p>
+          <p>Hair Accessories</p>
+          <p>Mens's Grooming</p>
+        </div>
+
+        <div className="">
+          <h5>PARTY SUPPLIES</h5>
+          <p>Event & Party</p>
+          <p>Christmas</p>
+          <p>Artificial Decorations</p>
+          <p>Wedding</p>
+        </div>
+
+        <div className="">
+          <h5>SPORT & OUTDOORS</h5>
+          <p>Team Sports</p>
+          <p>Water Sports</p>
+          <p>Outdoor Recreation</p>
+          <p>Fitness Equipment</p>
+        </div>
+
+        <div className="">
+          <h5>CLOTHES</h5>
+          <p>Bottoms</p>
+          <p>Women's Clothing</p>
+          <p>Artificial Decorations</p>
+          <p>Wedding</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubCategories;
