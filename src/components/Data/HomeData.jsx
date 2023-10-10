94bafc645b47412fe46a12f1af6dd19819da9ef9
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-10 23:04:26 +0000

    Update HomeData.jsx

---
+++ src/components/Data/HomeData.jsx
@@ -0,0 +1,38 @@
+import { reebok, patso, tonny, sketchers } from "../../assets";
+export const HomeData = [
+  {
+    id: 1,
+    name: "Tonny Black",
+    image: tonny,
+    tag: "Vegetable",
+    initialprice: "24.00",
+    sellingprice: "13.00",
+  },
+  {
+    id: 2,
+    name: "Reebok",
+    image: reebok,
+    details: "Women's power sneakers",
+    sellingprice: "112.02",
+    initialprice: "129.99",
+    discount: "-40%",
+  },
+  {
+    id: 3,
+    name: "Patso",
+    image: patso,
+    details: "Shoulder bag-pink-plain",
+    sellingprice: "69.99",
+    initialprice: "129.99",
+    discount: "-40%",
+  },
+  {
+    id: 4,
+    name: "Sketchers",
+    image: sketchers,
+    details: "Sport shoe-2102",
+    sellingprice: "80.99",
+    initialprice: "129.99",
+    discount: "-40%",
+  },
+];
