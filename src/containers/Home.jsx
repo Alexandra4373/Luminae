85d39d27944c7c0f30232de19198469f046b42fe
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-12 19:42:39 +0000

    Update Home.jsx

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,12 +11,12 @@ const Home = () => {
           <Navbar />
           <Categories />
 
-          <div className="container flex py-10 text-white">
+          <div className="container flex py-10 text-white p-0">
             <div className="flex-1 m-0">
               <img src={header} />
             </div>
 
-            <div className="flex-1 m-0 bg-primary  justify-center text-center ">
+            <div className="flex-1 m-0 bg-primary  justify-center text-center p-0 ">
               <h1>KIMONOS,CAFTANS & PAREOS</h1>
               <h4>Poolside glam included From $4.99</h4>
               <div className="flex">
@@ -27,11 +27,21 @@ const Home = () => {
               </div>
             </div>
           </div>
+
           <div className="">
+            <div className="">
+              <h4>Summer Essentials</h4>
+              <h5>20% off</h5>
+            </div>
+            <div className="bg-[#fff]">
+              <p>19 July-30 July</p>
+            </div>
+          </div>
+          {/* <div className="">
             <div className="">
               <h1 className="">Flash Sales</h1>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
