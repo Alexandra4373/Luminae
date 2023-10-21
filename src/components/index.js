83f778bdf081fb5d577411ff0cef5ceabebd6fa3
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-21 11:19:30 +0000

    updates

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Categories from "./Categories";
 import SubCategories from "./SubCategories";
+import Footer from "./Footer";
 
-export { Navbar, Categories, SubCategories };
+export { Navbar, Categories, SubCategories, Footer };
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,50 +1,52 @@
 // import React from 'react'
-import {Navbar, Categories} from "../components";
-import {header, shop} from "../assets";
+import { Navbar, Categories, Footer } from "../components";
+import { header, shop } from "../assets";
 // import HomeData from "../components/Data/HomeData";
 
 const Home = () => {
-	return (
-		<>
-			<Navbar />
+  return (
+    <>
+      <Navbar />
 
-			<Categories />
+      <Categories />
 
-			<div className="container flex text-white">
-				<div className="min-w-min">
-					<img src={header} className="" />
-				</div>
+      <div className="container flex text-white">
+        <div className="min-w-min">
+          <img src={header} className="" />
+        </div>
 
-				<div className="flex-1 bg-primary flex flex-col justify-center items-center">
-					<div className="text-center">
-						<h1>KIMONOS,CAFTANS & PAREOS</h1>
-						<h4>Poolside glam included From $4.99</h4>
-					</div>
-					<div className="">
-						<button type="button" className="flex gap-5 bg-second px-4 py-2">
-							<img src={shop} />
-							<p>SHOP NOW</p>
-						</button>
-					</div>
-				</div>
-			</div>
+        <div className="flex-1 bg-primary flex flex-col justify-center items-center">
+          <div className="text-center">
+            <h1>KIMONOS,CAFTANS & PAREOS</h1>
+            <h4>Poolside glam included From $4.99</h4>
+          </div>
+          <div className="">
+            <button type="button" className="flex gap-5 bg-second px-4 py-2">
+              <img src={shop} />
+              <p>SHOP NOW</p>
+            </button>
+          </div>
+        </div>
+      </div>
 
-			<div className="">
-				<div className="">
-					<h4>Summer Essentials</h4>
-					<h5>20% off</h5>
-				</div>
-				<div className="bg-[#fff]">
-					<p>19 July-30 July</p>
-				</div>
-			</div>
-			{/* <div className="">
+      <div className="">
+        <div className="">
+          <h4>Summer Essentials</h4>
+          <h5>20% off</h5>
+        </div>
+        <div className="bg-[#fff]">
+          <p>19 July-30 July</p>
+        </div>
+      </div>
+
+      <Footer />
+      {/* <div className="">
             <div className="">
               <h1 className="">Flash Sales</h1>
             </div>
           </div> */}
-		</>
-	);
+    </>
+  );
 };
 
 export default Home;
