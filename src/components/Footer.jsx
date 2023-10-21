719dd4082e14f4f1abfbd558f368667427a454ea
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-21 11:28:11 +0000

    Update Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="">
+        <ul>
+          <li>Company</li>
+          <li>About Us</li>
+          <li>Our Store</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+
+      <div className="">
+        <ul>
+          <li>Career Opportunities</li>
+          <li>Selling Programs</li>
+          <li>Advertise</li>
+          <li>Cooperation</li>
+        </ul>
+      </div>
+
+      <div className="">
+        <ul>
+          <li>How to Buy</li>
+          <li>Making Payments</li>
+          <li>Delivering</li>
+          <li>Buyer Protection</li>
+          <li>New User Guide</li>
+        </ul>
+      </div>
+
+      <div className="">
+        <ul>
+          <li>Help</li>
+          <li>Contact Us</li>
+          <li>FAQ</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
