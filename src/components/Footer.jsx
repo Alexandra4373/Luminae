ddbd3a98187b2d6a5154ba8f97d7fd41de9a67be
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-21 11:56:17 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,44 @@
-import React from "react";
-
 const Footer = () => {
   return (
     <div>
-      <div className="">
-        <ul>
-          <li>Company</li>
-          <li>About Us</li>
-          <li>Our Store</li>
-          <li>Contact us</li>
-        </ul>
-      </div>
-
-      <div className="">
-        <ul>
-          <li>Career Opportunities</li>
-          <li>Selling Programs</li>
-          <li>Advertise</li>
-          <li>Cooperation</li>
-        </ul>
-      </div>
-
-      <div className="">
-        <ul>
-          <li>How to Buy</li>
-          <li>Making Payments</li>
-          <li>Delivering</li>
-          <li>Buyer Protection</li>
-          <li>New User Guide</li>
-        </ul>
-      </div>
-
-      <div className="">
-        <ul>
-          <li>Help</li>
-          <li>Contact Us</li>
-          <li>FAQ</li>
-          <li>Privacy Policy</li>
-        </ul>
+      <div className=" container py-20 p-20">
+        <div className="flex gap-10">
+          <div className="flex-1 ">
+            <p className="lato text-[14px] font-bold mb-5">Company</p>
+            <ul className="lato text-[14px] font-normal ">
+              <li className="">About Us</li>
+              <li className="">Our Store</li>
+              <li className="">Contact us</li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <p className="lato text-[14px] font-bold mb-5">
+              Career Opportunities
+            </p>
+            <ul className="lato text-[14px] font-normal">
+              <li>Selling Programs</li>
+              <li>Advertise</li>
+              <li>Cooperation</li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <ul>
+              <li>How to Buy</li>
+              <li>Making Payments</li>
+              <li>Delivering</li>
+              <li>Buyer Protection</li>
+              <li>New User Guide</li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <ul>
+              <li>Help</li>
+              <li>Contact Us</li>
+              <li>FAQ</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{ts,tsx,js,jsx}"],
-	theme: {
-		colors: {
-			primary: "#70A4E9",
-			second: "#4172DC",
-			white: "#fff",
-			black: "#000",
-		},
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    colors: {
+      primary: "#70A4E9",
+      second: "#4172DC",
+      white: "#fff",
+      black: "#000",
+    },
 
-		fontFamily: {
-			open: ["Open Sans", "sans-serif"],
-			inter: ["inter", "sans-serif"],
-		},
-		extend: {
-			container: {
-				padding: "1rem",
-				center: true,
-			},
-			screens: {
-				sm: "640px",
-				md: "1060px",
-				lg: "1200px",
-			},
-		},
-	},
-	plugins: [],
+    fontFamily: {
+      open: ["Open Sans", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+      lato: ["Lato", "sans-serif"],
+    },
+    extend: {
+      container: {
+        padding: "1rem",
+        center: true,
+      },
+      screens: {
+        sm: "640px",
+        md: "1060px",
+        lg: "1200px",
+      },
+    },
+  },
+  plugins: [],
 };
