41729bc41b6ef01683d11e44fbad75114d2ff407
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-28 05:22:19 +0000

    Create NavData.jsx

---
+++ src/data/NavData.jsx
@@ -0,0 +1,6 @@
+export const NavData = [
+  { id: 1, name: "About us", link: "/" },
+  { id: 2, name: "Blog", link: "/blog" },
+  { id: 4, name: "Contact us", link: "/contact" },
+  { id: 6, name: "Help & Support", link: "/help" },
+];
