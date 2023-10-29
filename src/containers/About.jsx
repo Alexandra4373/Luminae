79e886c2fb17da522b03ddf8f573888cfeec1089
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-29 18:09:27 +0000

    Update About.jsx

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,11 +1,74 @@
 import React from "react";
-import { Navbar } from "../components";
+import { Navbar, Categories, Footer } from "../components";
+import { header } from "../assets";
 
 const About = () => {
   return (
     <div>
       <div className="">
         <Navbar />
+        <Categories />
+
+        <div className="">
+          <p>Homepage About us</p>
+        </div>
+
+        <div className="">
+          <p>Let's get to know Luminae</p>
+          <p>
+            Providing the best experience to make <br /> your{" "}
+            <span>Online Shopping</span>
+          </p>
+          <p>At Luminae,</p>
+          <div className="flex">
+            <div className="flex-1">
+              <p>20+</p>
+              <p>Shopping category</p>
+            </div>
+            <div className="flex-1">
+              <p>10+</p>
+              <p>Different Territory</p>
+            </div>
+            <div className="flex-1">
+              <p>4M+</p>
+              <p>Happy Client</p>
+            </div>
+          </div>
+
+          <div className="">
+            <p>Know Our Service</p>
+            <p>
+              We offer the best service <br /> that will make it easier
+            </p>
+            <p>Discover</p>
+          </div>
+        </div>
+
+        <div className="flex">
+          <div className="flex-1">
+            <p>
+              Tell us about you <br /> Concerns
+            </p>
+            <label>
+              <input type="text" placeholder="Enter your name here">
+                Your Name
+              </input>
+            </label>
+            <label>
+              <input type="text" placeholder="Enter your email here">
+                Email
+              </input>
+            </label>
+            <label>
+              <input type="Tell us about your concerns">Description</input>
+            </label>
+            <button>SEND</button>
+          </div>
+          <div className="flex-1">
+            <img src={header} />
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
