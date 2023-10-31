93c014957f964e9780cbc8736df4aff40f1b8a61
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-31 19:19:50 +0000

    Update Blog.jsx

--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -9,6 +9,34 @@ const Blog = () => {
           <img src={Logo} />
           <img src={Luminae} />
         </div>
+
+        <div className="">
+          <ul className=" flex gap-10">
+            <li>
+              <a href="#About">NEWS</a>
+            </li>
+            <li>
+              <a href="#Blog">FASHION</a>
+            </li>
+            <li>
+              <a href="#Contact Us">HEALTH</a>
+            </li>
+            <li>
+              <a href="#Help & Support">TECH</a>
+            </li>
+            <li>
+              <a href="#Help & Support">SCIENCE</a>
+            </li>
+          </ul>
+        </div>
+
+        <p>Stunning barefooted woman of 2023</p>
+
+        <p>
+          In the heart of a chic urban studio, the atmosphere buzzed with
+          excitement as a trendy fur coat took center stage during a vibrant
+          photoshoot.
+        </p>
       </div>
     </div>
   );
